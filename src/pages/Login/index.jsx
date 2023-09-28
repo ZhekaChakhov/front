7b5e8f629fe468e6c41cff02c8dc8108ae0f73ba
Zheka Chakhov <zhekachakhov@mail.ru>
2023-09-28 13:48:59 +0300

    made auth check add logout function

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
@@ -7,9 +9,10 @@ import Button from "@mui/material/Button";
 import { useForm } from "react-hook-form";
 
 import styles from "./Login.module.scss";
-import { fetchAuth } from "../../redux/slices/auth";
+import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
 
 export const Login = () => {
+	const isAuth = useSelector(selectIsAuth);
 	const dispatch = useDispatch();
 
 	const {
@@ -27,7 +30,11 @@ export const Login = () => {
 	const onSubmit = (values) => {
 		dispatch(fetchAuth(values));
 	};
-	console.log(errors, isValid);
+
+	if (isAuth) {
+		// если авторизованы - перенаправить нас на главную страницу
+		return <Navigate to="/" />;
+	}
 
 	/* если два поля {...register(...)} рендерятся
         то мы их сразу регистрируем в useForm, и он их будет обрабатывать */
